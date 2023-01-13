e258691698b8d95c5af607491657ba403b39515d
Eddy Arellanes <developer@eddyarellanes.dev>
2023-01-13 10:55:31 -0600

    chore: :pencil: add gql queries as example

--- backend/graphql/index.ts
+++ backend/graphql/index.ts
@@ -1,18 +1,28 @@
 import { ApolloServer } from 'apollo-server-express';
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
-
+// != not null
 const typeDefs = `
   type Query {
-    hello: String,
-    getPerson(name: String, age: Int): String
+    hello: String!,
+    getPerson(name: String, age: Int!): String
+    getInt: Int
+    getFloat: Float
+    getString: String
+    getBoolean: Boolean
+    getID: ID
   }
 `
 
-//esolvers receives 4 params
+//Resolvers receives 4 params
 const resolvers = {
   Query: {
     hello: () => 'hello world',
-    getPerson: (_, args) => `Hello my name is ${args.name}`
+    getPerson: (_, args) => `Hello my name is ${args.name}`,
+    getInt: () => 1,
+    getFloat: () => 1.5,
+    getString: () => "Hello again",
+    getBoolean: () => true,
+    getID: () => '1010101'
   }
 }
 
